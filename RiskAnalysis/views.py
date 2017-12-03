2ffb2dce03ceced03c94916d91fe8efed3c8abfc
Martin Zhao <findns94@gmail.com>
2017-12-03 16:42:56 +0800

    update platform info

--- RiskAnalysis/views.py
+++ RiskAnalysis/views.py
@@ -1,3 +1,5 @@
+# -*- coding: UTF-8 -*-
+
 import random
 
 from django.core.serializers import json
@@ -5,7 +7,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-from RiskAnalysis.models import Platform
+from RiskAnalysis.models import *
 
 
 def platform_region(request):
@@ -24,19 +26,29 @@ def platform_region(request):
     for region, list_business in dict_region.items():
         length = len(list_business)
 
+        # 一个省只有一个平台
         if length == 1:
             response_data += '{"source":"' + list_business[0] + '","target":"' + list_business[
                 0] + '","region":"' + region + '"},\n'
             continue
+        else:
+            center_platform = list_business[0]
+            for i in range(1,length):
+                branch_platorm = list_business[i]
+                response_data += '{"source":"' + center_platform + \
+                                 '","target":"' + branch_platorm + \
+                                 '","region":"' + region + '"},\n'
 
-        for i in range(length - 1):
-            for j in range(i + 1, length):
-                name_i = list_business[i]
-                name_j = list_business[j]
-                if random.randint(0,100) % 5 != 0 and j > 1 and i > 0:
-                    pass
-                else:
-                    response_data += '{"source":"' + name_i + '","target":"' + name_j + '","region":"' + region + '"},\n'
+            # for i in range(length - 1):
+            #     for j in range(i + 1, length):
+            #         name_i = list_business[i]
+            #         name_j = list_business[j]
+            #         if random.randint(0, 100) % 5 != 0 and j > 1 and i > 0:
+            #             pass
+            #         else:
+            #             response_data += '{"source":"' + name_i + \
+            #                              '","target":"' + name_j + \
+            #                              '","region":"' + region + '"},\n'
 
     response_data = response_data[:-2] + '\n'
 
@@ -46,11 +58,11 @@ def platform_region(request):
 
 
 def home(request):
-    # list_platform = Platform.objects.all()
+    list_wdzj = Wdzjinfo.objects.filter(enddate='2017-10-31')
     #
     # for item in list_platform:
     #     print(item.name, item.region)
-    return render(request, 'home.html')
+    return render(request, 'home.html',{'list_wdzj': list_wdzj})
 
 def platform(request):
     # list_platform = Platform.objects.all()
